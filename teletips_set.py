74c2f12b819aa985f081ea0493ec8b268af13b79
Comrade X <106185020+comrade09@users.noreply.github.com>
2024-01-04 00:15:50 +0530

    Update teletips_set.py

--- teletips_set.py
+++ teletips_set.py
@@ -9,7 +9,8 @@
 # ##Credits - [Countdown Timer Telegram bot by TeLe TiPs] (https://github.com/teletips/CountdownTimer-TeLeTiPs)
 
 # Changing the code is not allowed! Read GNU AFFERO GENERAL PUBLIC LICENSE: https://github.com/teletips/CountdownTimer-TeLeTiPs/blob/main/LICEN
-from urllib3.contrib.appengine import is_appengine_sandbox
+import urllib3
+from urllib3 import is_appengine_sandbox
 import os
 from pyrogram import Client, filters
 from pyrogram import enums
